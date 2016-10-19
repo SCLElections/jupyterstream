1cb52d476604f4a6343650a8ba0850ae6b8b38f5
Eyal <eyalkazin@gmail.com>
2016-10-19 18:44:10 +0100

    Update demo_path.py

--- demo/demo_path.py
+++ demo/demo_path.py
@@ -1,4 +1,4 @@
 # For the Demo to work you need to change to the parent directory "jupyterstream", 
-# I.e the directory ihh which README.md is.
+# I.e the directory in which README.md is.
 path = "/change/your/path/to/jupyterstream/"
 path +="/"
