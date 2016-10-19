3d8ce48d6aec394dfc0bee307bda2584a776d1c9
Eyal Kazin <eyalkazin@gmail.com>
2016-10-19 18:42:39 +0100

    re-pathing the demo. currently tested runipy_wrapper (one value)

--- demo/demo_path.py
+++ demo/demo_path.py
@@ -1,1 +1,4 @@
-path = "/home/eyalk/projects/blog/public/jupyterstream/"
+# For the Demo to work you need to change to the parent directory "jupyterstream", 
+# I.e the directory ihh which README.md is.
+path = "/change/your/path/to/jupyterstream/"
+path +="/"
